34e837568fb3708af33c06bdd861ba21a2047a31
Willyan Marques <willyanmvs@gmail.com>
2020-07-17 02:43:04 -0300

    Updates

---
+++ precache-manifest.dc042f64a6dc84185d3df1201c94cb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c504db996f1beaca193df3702cfd775",
+    "url": "/salesforce-migration/index.html"
+  },
+  {
+    "revision": "ae42199c8e22d4344892",
+    "url": "/salesforce-migration/static/css/main.973dd558.chunk.css"
+  },
+  {
+    "revision": "9c2a0b4e18f01ee3476a",
+    "url": "/salesforce-migration/static/js/2.e73ee59d.chunk.js"
+  },
+  {
+    "revision": "3d6c976ecf99a95ef06a2ff0f39f94f4",
+    "url": "/salesforce-migration/static/js/2.e73ee59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae42199c8e22d4344892",
+    "url": "/salesforce-migration/static/js/main.5cc42d51.chunk.js"
+  },
+  {
+    "revision": "a493cc606755a6e8d488",
+    "url": "/salesforce-migration/static/js/runtime-main.e0ce5c62.js"
+  }
+]);
